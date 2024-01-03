3d8b240d06d91ae7c623dc7067342d4471b87724
John Karuntzos <61172885+John-Karuntzos@users.noreply.github.com>
2024-01-03 03:22:29 +0000

    completed twofer assignment

---
+++ twofer.rb
@@ -0,0 +1,12 @@
+
+#######################################################
+name = ["raghu", "Bob", ""].sample
+#######################################################
+# write your program below
+
+input = "you"
+if name != ""
+  input = name.capitalize
+end
+
+pp "One for #{input}, one for me."
